6a88d81434a7b62f062385fa96c8563006c7019a
SARAH BARRON <sarahibarron@hotmail.co.uk>
2023-10-03 09:42:35 +0100

    amended js

--- app.js
+++ app.js
@@ -1,87 +1,74 @@
+const img1 = document.getElementById(img1)
+const img2 = document.getElementById(img2)
+const img3 = document.getElementById(img3)
+
+let userClicks = 0;
+let maxClicks = 25;
+
 // Create a constructor function that creates an object associated with each product
-function Product(name, src, imgCount) {
+function Product(name) {
   this.name = name;
-  this.src = src;
-  this.imgCount = imgCount;
+  this.src = `./images/${name}.jpg`;
+  this.views = 0;
+  this.clicks = 0;
 }
 
-// Create an algorithm that will randomly generate three unique product images from the images directory and display them side-by-side-by-side in the browser window.
-function renderProducts() {
-  let product1Index = getRandomIndex();
-  let product2Index = getRandomIndex();
-  let product3Index = getRandomIndex();
-}
+const products = [
+  new Product("bag"),
+  new Product("banana"),
+  new Product("bathroom"),
+  new Product("bag"),
+  new Product("bag"),
+  new Product("bag"),
+  new Product("bag"),
+];
 
-// For each of the three images, increment its property of times it has been shown by one.
+console.log(products);
 
-// Attach an event listener to the section of the HTML page where the images are going to be displayed.
-
-// Once the users ‘clicks’ a product, generate three new products for the user to pick from.
-
-// function to choose a random product
-function getRandomIndex() {
+function randomProdIdx() {
   return Math.floor(Math.random() * allProducts.length);
 }
 
-// function to render 2 random products
 function renderProducts() {
-  // get 2 rnadom indexes from our product array
-  let product1Index = getRandomIndex();
-  let product2Index = getRandomIndex();
-
-  // prevent the two images being the same product
-  while (product1Index === product2Index) {
-    product2Index = getRandomIndex();
-  }
-
-  image1.src = allProducts[product1Index].src;
-  image2.src = allProducts[product2Index].src;
-  image1.alt = allProducts[product1Index].name;
-  image2.alt = allProducts[product2Index].name;
-
-  allProducts[product1Index].views++;
-  allProducts[product2Index].views++;
+  let prod1 = randomProdIdx();
+  let prod2 = randomProdIdx();
+  let prod3 = randomProdIdx();
 }
 
-function handleProductClick(event) {
-  let clickedProduct = event.target.alt;
-
-  // check if the click is on an image
-  if (event.target === ProductContainer) {
-    alert("Please click on an image");
-  } else {
-    renderProducts();
-  }
-
-  // increase the clicks of the product
-  // loop through allProducts
-  for (let i = 0; i < allProducts.length; i++) {
-    // check if the name of the product in the array, matches the alt tag of our image
-    if (clickedProduct === allProducts[i].name) {
-      // increase the number of clicks
-      allProducts[i].clicks++;
-      // stop the for loop because we found the product
-      break;
-    }
-  }
+while (prod1 === prod2 || prod1 === prod3 || prod2 === prod3) {
+  prod2 = randomProdIdx();
+  prod3 = randomProdIdx();
 }
 
-// make the products
-const allProducts = [
-  new Product("bag", "./images/bag.jpg"),
-  new Product("bag", "./images/bag.jpg"),
-  new Product("bag", "./images/bag.jpg"),
-  new Product("bag", "./images/bag.jpg"),
-  new Product("bag", "./images/bag.jpg"),
-  new Product("bag", "./images/bag.jpg"),
-  new Product("bag", "./images/bag.jpg"),
-  new Product("bag", "./images/bag.jpg"),
-  new Product("bag", "./images/bag.jpg"),
-  new Product("bag", "./images/bag.jpg"),
-  new Product("bag", "./images/bag.jpg"),
-  new Product("bag", "./images/bag.jpg"),
-];
+img1.src = products[prod1].src;
+img2.src = products[prod2].src;
+img3.src = products[prod3].src;
+img1.alt = products[prod1].name;
+img2.alt = products[prod2].name;
+img3.alt products[prod3].name;
 
-productContainer.addEventListener("click", handleProductClick);
+products[prod1].views++;
+products[prod2].views++;
+products[prod3].views++;
 
 renderProducts();
+
+function handleImgClick(event) {
+  let clickedProduct = event.target.alt;
+  for(let i = 0; i < products.length; i++){
+    if(clickedProduct === products[i].name){
+      products[i].clicks++ ;
+      break 
+    }
+  }
+  renderProducts();
+
+  if(userClicks === maxClicks){
+    console.log ("Maximum Clicks");
+  }
+
+}
+
+img1.addEventListener("click" , handleImgClick);
+img2.addEventListener("click" , handleImgClick);
+img3.addEventListener("click" , handleImgClick);
